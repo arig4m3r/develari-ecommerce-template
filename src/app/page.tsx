d0c2eda312474b6060206648ac835f21a012c104
Ari Rocha <ari@arirocha.com.br>
2024-07-18 01:41:34 -0300

    Made the first page an async page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const date = new Date();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -8,6 +9,12 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
+
+        <div className="flex gap-4">
+          Data de hoje:{" "}
+          <span className="font-bold">{date.toLocaleString("pt-BR")}</span>
+        </div>
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
